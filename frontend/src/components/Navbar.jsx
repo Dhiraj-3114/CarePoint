75e11da2151b18f303e5dfdc41d1d9c3167e5d09
dhiraj Patel <pateldhiraj917@gmail.com>
2025-04-08 14:30:58 +0530

    completed doctors page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 borderd-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={assets.logo_2} alt="" />
+        <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo_2} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
