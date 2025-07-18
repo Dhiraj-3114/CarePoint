9ea05b2fbd759fa8769c0f5795f1957a77a45be9
dhiraj Patel <pateldhiraj917@gmail.com>
2025-07-18 23:54:37 +0530

    Show my all appointment and Appointment cancel functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -193,11 +193,63 @@ const bookAppointment = async (req, res) => {
 
 }
 
+// api to get list of user's appointments
+
+const listAppointment = async (req, res) => {
+
+    try {
+
+        const userId = req.userId
+        const appointments = await appointmentModel.find({userId})
+
+        res.json({success: true, appointments}) 
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})     
+    }
+
+}
+
+const cancelAppointment = async (req, res) => {
+
+    try {
+
+        const userId = req.userId
+        const {appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        if (appointmentData.userId !== userId) {
+            return res.json({success: false, message: 'Unauthorized Action !!'}) 
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled: true})
+
+        const {docId, slotDate, slotTime} = appointmentData
+        const doctorData = await doctorModel.findById(docId)
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success: true, message: 'Appointment Cancelled !!'})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})   
+    }
+
+}
+
 
 export {
     registerUser, 
     loginUser, 
     getProfile, 
     updateProfile, 
-    bookAppointment
+    bookAppointment,
+    listAppointment,
+    cancelAppointment,
 }
