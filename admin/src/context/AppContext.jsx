06b82f19be39917b7f695c865ed14f4d51304a09
dhiraj Patel <pateldhiraj917@gmail.com>
2025-07-26 13:57:34 +0530

    Admin side Appointments section completely done

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,8 +4,27 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const value = {
+    const currency = '$'
 
+    const calculateAge = (dob) => {
+        const birthDate = new Date(dob)
+        const today = new Date()
+        let age = today.getFullYear() - birthDate.getFullYear()
+
+        return age 
+    }
+
+    const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    const slotDateFormat = (slotDate) => {
+        const dateArray = slotDate.split('/')
+        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    }
+
+    const value = {
+        currency,
+        calculateAge,
+        slotDateFormat, 
     }
 
     return (
