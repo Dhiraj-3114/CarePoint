967092da9e31aff861b4af9efc78cc8aa66ddba2
dhiraj Patel <pateldhiraj917@gmail.com>
2025-07-27 14:08:51 +0530

    Doctor login functionality added

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { doctorList } from '../controllers/doctorController.js'
+import { doctorList, loginDoctor } from '../controllers/doctorController.js'
 
 const doctorsRouter = express.Router() 
 
 doctorsRouter.get('/list', doctorList)
+doctorsRouter.post('/login', loginDoctor) 
 
 export default doctorsRouter 
