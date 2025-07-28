8b64348cc6423b2e8af2430815038a6e5e9c7c50
dhiraj Patel <pateldhiraj917@gmail.com>
2025-07-29 01:15:38 +0530

    Some changes in fronted

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -133,10 +133,11 @@ const MyAppointments = () => {
               </div>
               <div></div>
               <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && item.payment && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>} 
-                {!item.cancelled && !item.payment && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-amber-600 hover:text-white transition-all duration-300'>Pay Online</button>}
-                {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
-                {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-600 rounded text-red-600 cursor-pointer'>Appointment Cancelled</button>}
+                {!item.cancelled && item.payment && !item.isCompleted && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>} 
+                {!item.cancelled && !item.payment && !item.isCompleted && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-amber-600 hover:text-white transition-all duration-300'>Pay Online</button>}
+                {!item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+                {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-600 rounded text-red-600 cursor-pointer'>Appointment Cancelled</button>}
+                {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-600 rounded text-green-600 cursor-pointer'>Completed</button>}
               </div>
             </div>
           ))
